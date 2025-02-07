9f40de1832b971f325e9a76b3d94ebcb72cb8a80
Anderson Krahl <ander_uo@hotmail.com>
2025-02-07 18:26:30 -0300

    bugado

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,14 @@ FROM node:22
 WORKDIR /usr/src/app
  
 COPY package.json yarn.lock ./
-RUN yarn install
+RUN yarn install --frozen-lockfile
+
+COPY prisma/schema.prisma ./prisma/
+RUN npx prisma generate
  
 COPY . .
  
 RUN yarn build
  
 EXPOSE 8080
-CMD [ "yarn", "start" ]
+CMD [ "yarn", "start", "--memory=1g", "--memory-swap=2g" ]
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
-import morgan from 'morgan';const app = express();
-app.use(morgan('dev')); // logger
- 
-app.get('/', async (req, res) => {
-  res.json({ hello: "world" });
-});
- 
-const port = Number(process.env.PORT ?? 8080);
+import morgan from 'morgan';
+import db from './modules/db';
 
+const app = express();
+app.use(morgan('dev')); // logger
+
+app.get('/', async (req, res) => {
+  const pets = await db.pet.findMany();
+  res.json(pets);
+});
+
+const port = Number(process.env.PORT ?? 8080);
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${port}`);
 });
