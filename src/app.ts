b71cf2ae47837797fab72ba593d5a3b059b32f8c
arthurwieb <arthur.wieb@gmail.com>
2025-02-25 22:50:40 -0300

    comentario

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import routes from './routes';
 import prismaPlugin from './plugins/prisma.plugin';
 
 export default function buildApp() {
+  // colocar logger: true se quiser debugar hard
   const app = fastify();
 
   // Register the Prisma plugin
