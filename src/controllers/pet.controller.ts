702c8bdf47e9948f72525509e0e211f0fd972b7c
arthurwieb <arthur.wieb@gmail.com>
2025-03-25 20:38:36 -0300

    ajuste em nome de classes

--- src/controllers/pet.controller.ts
+++ src/controllers/pet.controller.ts
@@ -2,8 +2,8 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { Prisma } from '@prisma/client';
 
 export class PetController {
-  static async createPet(request: FastifyRequest<{ Body: Prisma.PetCreateInput}>, reply: FastifyReply) {
-     try {
+  static async createPet(request: FastifyRequest<{ Body: Prisma.PetCreateInput }>, reply: FastifyReply) {
+    try {
       const pet = await request.server.prisma.pet.create({ data: request.body });
       return reply.send(pet);
     } catch (error) {
@@ -12,42 +12,42 @@ export class PetController {
   }
 
 
-  static async getPets (request: FastifyRequest, reply: FastifyReply) {
+  static async getPets(request: FastifyRequest, reply: FastifyReply) {
     try {
       const pets = await request.server.prisma.pet.findMany();
       return reply.send(pets);
     } catch (error) {
-      return reply.status(500).send({ error: 'Failed to fetch pets'});
+      return reply.status(500).send({ error: 'Failed to fetch pets' });
     }
   }
 
-  static async getPetById (request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply) {
+  static async getPetById(request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply) {
     try {
       const pet = await request.server.prisma.pet.findUnique({
         where: { id: parseInt(request.params.id, 10) },
       });
       pet ? reply.send(pet) : reply.status(404).send({ error: 'Pet not found' })
-    } catch ( error ) {
+    } catch (error) {
       reply.status(500).send({ error: 'Failed to fetch pet' })
     }
   }
 
-  static async deletePet (request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply) {
+  static async deletePet(request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply) {
     try {
       const deletePet = await request.server.prisma.pet.findUnique({
         where: { id: parseInt(request.params.id, 10) },
       });
-      return reply.send({ message: 'Pet deleted successfully'})
-    } catch ( error ) {
+      return reply.send({ message: 'Pet deleted successfully' })
+    } catch (error) {
       reply.status(500).send({ error: 'Failed to delete pet' });
     }
   }
 
-  static async updatePet (request: FastifyRequest<{ Params: { id: string }, Body: { name?: string } }>, reply: FastifyReply) {
+  static async updatePet(request: FastifyRequest<{ Params: { id: string }, Body: { name?: string } }>, reply: FastifyReply) {
     try {
       const { name } = request.body
       const updatePet = await request.server.prisma.pet.findUnique({
-        where: {id: parseInt(request.params.id, 10) },
+        where: { id: parseInt(request.params.id, 10) },
       });
       updatePet ? reply.send(updatePet) : reply.status(404).send({ error: 'Pet not found' })
 
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,8 +2,8 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { Prisma } from '@prisma/client';
 
 export class UserController {
-  static async createUser(request: FastifyRequest<{ Body: Prisma.UserCreateInput}>, reply: FastifyReply) {
-     try {
+  static async createUser(request: FastifyRequest<{ Body: Prisma.UserCreateInput }>, reply: FastifyReply) {
+    try {
       const user = await request.server.prisma.user.create({ data: request.body });
       return reply.send(user);
     } catch (error) {
