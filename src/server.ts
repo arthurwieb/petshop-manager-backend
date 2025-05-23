cf8d9e585c9608b789584d8c95644fc18a947254
arthur wiebusch <arthur.wieb@gmail.com>
2025-05-22 23:01:59 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,10 @@ const app = buildApp();
 
 const start = async () => {
   try {
-    await app.listen({ port: 3001, host: "0.0.0.0" });
-    console.log("Server running on http://localhost:3001");
+    await app.ready();
+    console.log(app.printRoutes());
+    await app.listen({ port: 3001, host: '0.0.0.0' });
+    console.log('Server running on http://localhost:3001');
   } catch (err) {
     app.log.error(err);
     process.exit(1);
